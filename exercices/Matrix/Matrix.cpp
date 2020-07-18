a8f857d207b47d08b541796d2cad29a30a57c53c
Cássio A. T. Alves <43801697+RedSlifer@users.noreply.github.com>
2020-07-17 22:06:29 -0400

    Add files via upload

---
+++ exercices/Matrix/Matrix.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include "Matrix.h"
+
+Matrix::Matrix()
+{
+   this -> elements.reserve(0);
+}
+
+std::vector<std::vector<unsigned int>> Matrix::matrix_builder(const unsigned int& rows, const unsigned int& columns)
+{
+    unsigned int i, j, elements;
+    std::vector<unsigned int> row;
+
+    std::cout << "Enter the elements" << std::endl;
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < columns; j++)
+        {
+            std::cin >> elements;
+            row.push_back(elements);
+        }
+        this -> elements.push_back(row);
+        row.clear();
+    }
+    return this -> elements;
+}
+
+unsigned int Matrix::sum_elements()
+{
+    unsigned int sum = 0;
+
+    for(const auto& rows : this -> elements)
+    {
+      for(auto element : rows)
+      {
+        sum = sum + element;
+      }
+    }
+
+    return sum;
+}
+
+void Matrix::matrix_printer()
+{
+    for (const auto& rows : this -> elements)
+    {
+        for(auto element : rows)
+        {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ exercices/SinglyLinkedList/SinglyLinkedList.h
@@ -0,0 +1,33 @@
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+
+template <typename T>
+class SinglyLinkedList
+{
+private:
+    struct Node
+    {
+        T element;  // Element saved by the node
+        Node* link;  // Link to the next node
+    };
+    Node* head_link; // Head node of the list
+
+public:
+    SinglyLinkedList();
+    ~SinglyLinkedList();
+
+public:
+    bool is_empty();
+	int lenght() const; // to do
+	bool search(const T& element); // to do
+    T get_front() const;
+    void insert_front(const T& element); 
+	void insert_back(const T& element); // to do
+    void remove_front();
+	void remove_back(); // to do
+	void remove_node(int position); // to do
+    void print();
+	void reverse_print(Node* jumper = this -> head_link);
+};
+
+#endif
