ee31d616964d387b8e9e77fc412bb3987cb6bec0
Cássio A. T. Alves <43801697+RedSlifer@users.noreply.github.com>
2021-04-11 21:38:19 -0400

    Update SinglyLinkedList.h

--- exercices/SinglyLinkedList/SinglyLinkedList.h
+++ exercices/SinglyLinkedList/SinglyLinkedList.h
@@ -18,16 +18,16 @@ public:
 
 public:
     bool is_empty();
-	int lenght() const; // to do
-	bool search(const T& element); // to do
+    int lenght() const; // to do
+    bool search(const T& element); // to do
     T get_front() const;
     void insert_front(const T& element); 
-	void insert_back(const T& element); // to do
+    void insert_back(const T& element); // to do
     void remove_front();
-	void remove_back(); // to do
-	void remove_node(int position); // to do
+    void remove_back(); // to do
+    void remove_node(int position); // to do
     void print();
-	void reverse_print(Node* jumper = this -> head_link);
+    void reverse_print(Node* jumper = this -> head_link);
 };
 
 #endif
